500d40d8d124d6506917b21cd3ccc7bceef2a1a3
xXTeraXx <57876649+xXTeraXx@users.noreply.github.com>
2020-01-17 18:18:45 -0500

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+sudo apt update
+sudo apt upgrade
+sudo apt install openjdk-8-jdk
+sudo apt install screen
+cd /home/pi
+sudo mv Raspi-Minecraft-Server Minecraft_Server
+cd Minecraft_Server
+wget https://getbukkit.org/get/1eb34a7d4db660bb88d02b6c8845957c
+sudo mv spigot-1.15.1.jar server.jar
+cd /etc
+sudo rm rc.local
+cd /home/pi/Minecraft_Server
+sudo mv rc.local /etc
+cd /etc
+chmod +x rc.local
