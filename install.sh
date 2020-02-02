a0682480216be4deed23b1ea4efc9ba59305f429
xXTeraXx <57876649+xXTeraXx@users.noreply.github.com>
2020-02-01 21:37:09 -0500

    Update install sh to 1.15.2

--- install.sh
+++ install.sh
@@ -6,8 +6,8 @@ sudo apt install screen
 cd /home/pi
 sudo mv Raspi-Minecraft-Server Minecraft_Server
 cd Minecraft_Server
-wget https://cdn.getbukkit.org/spigot/spigot-1.15.1.jar
-sudo mv spigot-1.15.1.jar server.jar
+wget https://cdn.getbukkit.org/spigot/spigot-1.15.2.jar
+sudo mv spigot-1.15.2.jar server.jar
 cd /etc
 sudo rm rc.local
 cd /home/pi/Minecraft_Server
